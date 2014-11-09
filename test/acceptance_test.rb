c350559309853a1ad39c2cc10855a23cbb032723
Kalah Cheek <kcheek7@ymail.com>
2014-11-08 23:54:02 -0700

    create acceptance test

--- test/acceptance_test.rb
+++ test/acceptance_test.rb
@@ -1,35 +1,79 @@
-rrgb
-> ruby mastermind.rb
-Welcome to Mastermind!
-type 'p'or 'play' to play,
-'i' or 'instructions' for instructions,
-'q'or 'quit' to quit
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require '../lib/cli'
 
-please input a command: p
+class AcceptanceTest < Minitest::Test
+  class FakeStdin
+    def initialize(*inputs)
+      @inputs = inputs         # => ["a\n", "b\n"]
+      @index = 0               # => 0
+    end
 
-Guess the four color secret sequence.
-You can input 'r' for red, 'b' for blue, 'g' for green, and 'y' for yellow.
-You can use any sequence and any number of each colors up to four.
+    def gets
+      input = @inputs[@index]  # => "a\n", "b\n", nil
+      @index += 1              # => 1, 2, 3
+      input                    # => "a\n", "b\n", nil
+    end
+  end
 
-please input a guess: rrrr
+  class FakeStdout
 
-You have 1 color correct.
-You have 2 in the correct spot.
-You have made 1 guess.
+    def printed
+      @printed || @printed = []  # => [], [], ["a"], ["a"], ["a", "b"]
+    end
 
-please input a guess: rrggg
-You have entered too many characters.
-please input a guess: rrgg
+    def puts(message)
+      printed << message         # => ["a"], ["a", "b"]
+    end
 
-You have 2 colors correct.
-You have 3 in the correct spot.
-You have made 2 guesses.
+  end
 
-please input a guess: rrgb
+  def test_acceptance
+    stdin  = FakeStdin.new("p\n", "rrrr\n", "rrggg\n", "rrgg\n", "rrgb\n", "quit\n")
+    stdout = FakeStdout.new
+    cli    = CLI.new(stdin, stdout, "rrgb")
 
-You've won!  Congratulations!
-It took you 3 guesses to complete the game.
+    # > ruby mastermind.rb
+    cli.call
+    # greets user
+    assert_includes stdout.printed, "Welcome to Mastermind!"
 
-Do you want to play again or quit? quit
+    # informs user of their options
+    assert_includes stdout.printed, "type 'p'or 'play' to play,"
 
-Goodbye, and have a lovely day!
+    # prompts for a command (p)
+    assert_includes stdout.printed, "please input a command: "
+
+    # prompts for color
+    assert_includes stdout.printed, "Guess the four color secret sequence."
+
+    # prompts for a guess (rrrr)
+    assert_includes stdout.printed, "please input a guess: "
+
+    # tells us the correct number
+    assert_includes stdout.printed, "You have 1 color correct."
+    assert_includes stdout.printed, "You have 2 in the correct spot."
+
+    # tells us how many we guessed
+    assert_includes stdout.printed, "You have made 1 guess."
+
+    # identifies the incorrect guess (rrggg)
+    assert_includes stdout.printed, "You have entered too many characters."
+
+    # takes another guess
+    assert_includes stdout.printed, "You have made 2 guesses."
+
+    # identifies the win (rrgb)
+    assert_includes stdout.printed, "You've won!  Congratulations!"
+
+    # tells us how many guesses it took
+    assert_includes stdout.printed, "It took you 3 guesses to complete the game."
+
+    # allows us to play again if we like
+    assert_includes stdout.printed, "Do you want to play again or quit? "
+
+    # bids us farewell
+    assert_includes stdout.printed, "Goodbye, and have a lovely day!"
+  end
+end
