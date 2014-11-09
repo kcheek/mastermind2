238ebdab6802891e2c40a5312e02e04515f8c4cb
Kalah Cheek <kcheek7@ymail.com>
2014-11-08 20:53:47 -0700

    initial commit

---
+++ test/acceptance_test.rb
@@ -0,0 +1,35 @@
+rrgb
+> ruby mastermind.rb
+Welcome to Mastermind!
+type 'p'or 'play' to play,
+'i' or 'instructions' for instructions,
+'q'or 'quit' to quit
+
+please input a command: p
+
+Guess the four color secret sequence.
+You can input 'r' for red, 'b' for blue, 'g' for green, and 'y' for yellow.
+You can use any sequence and any number of each colors up to four.
+
+please input a guess: rrrr
+
+You have 1 color correct.
+You have 2 in the correct spot.
+You have made 1 guess.
+
+please input a guess: rrggg
+You have entered too many characters.
+please input a guess: rrgg
+
+You have 2 colors correct.
+You have 3 in the correct spot.
+You have made 2 guesses.
+
+please input a guess: rrgb
+
+You've won!  Congratulations!
+It took you 3 guesses to complete the game.
+
+Do you want to play again or quit? quit
+
+Goodbye, and have a lovely day!
