5d729405e560de307e07dcb906154036d7b33d7c
Kalah Cheek <kcheek7@ymail.com>
2014-11-09 18:48:56 -0700

    define methods too long, too short, invalid input

--- lib/input.rb
+++ lib/input.rb
@@ -1,29 +1,44 @@
 class Input
+  attr_reader :input
 
   def initialize(input)
     @input = input
   end
 
-  def input
-    @input
+  def quit?
+    input == 'q' || input == 'quit'
+  end
+
+  def instructions?
+    input == 'i' || input == 'instructions'
+  end
+
+  def play?
+    input == 'p' || input == 'play'
   end
 
   def invalid_input?
+    input[/[^rgbyq]/] && input != 'quit'
+  end
 
+  def improper_input_length?
+    input_too_long || input_too_short
   end
 
   def input_too_long?
-
+    input.length > 4
   end
 
   def input_too_short?
-
+    input.length < 4
   end
 end
-# accept input and differentiate between command
-# identify if guess is too short
-# identify if guess is too long
+
 # identify if guess is invalid
-# identify if guess is quit?
-# identify if guess is instructions?
-# identify if guess is play?
+
+# accept input and differentiate between command-
+# identify if guess is too short-
+# identify if guess is too long-
+# identify if guess is quit?-
+# identify if guess is instructions?-
+# identify if guess is play?-
