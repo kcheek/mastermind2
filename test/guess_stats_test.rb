62fa6c05e87897fa9e366b7109c8c5da4c162a43
Kalah Cheek <kcheek7@ymail.com>
2014-11-09 18:49:26 -0700

    create tests

---
+++ test/guess_stats_test.rb
@@ -0,0 +1,42 @@
+require_relative 'test_helper'
+
+class CorrectSpotTest < Minitest::Test
+
+  def assert_correct_spot(secret_code, guessed_colors, num)
+    stats = GuessStats.new(secret_code, guessed_colors)
+    assert_equal num, stats.correct_spot
+  end
+
+  def test_how_many_are_in_the_correct_spot_and_color
+    assert_correct_spot "bbbb", "bbbg", 3
+    assert_correct_spot "bbbb", "bbgg", 2
+    assert_correct_spot "bbbb", "bggg", 1
+    assert_correct_spot "bbbb", "gggg", 0
+    assert_correct_spot "bbbg", "gggb", 0
+    assert_correct_spot "bbbb", "bbbb", 4
+  end
+
+  def test_it_does_not_accept_invalid_letters
+    assert_correct_spot "bbbb", "bbbx", 0
+    assert_correct_spot "bbbb", "xxxx", 0
+  end
+
+end
+
+class CorrectColorTest < Minitest::Test
+
+  def assert_correct_color(secret_code, guessed_colors, num)
+    stats = GuessStats.new(secret_code, guessed_colors)
+    assert_equal num, stats.correct_color
+  end
+
+  def test_how_many_are_the_correct_color
+    assert_correct_color "byrg", "bbbb", 1
+    assert_correct_color "byrg", "gggb", 2
+    assert_correct_color "byrg", "ygbg", 3
+    assert_correct_color "byrg", "bygr", 4
+    assert_correct_color "bbgg", "bggr", 2
+    assert_correct_color "bbgg", "ggbb", 2
+  end
+
+end
--- test/input_test.rb
+++ test/input_test.rb
@@ -8,19 +8,22 @@ class TestInputValidity < Minitest::Test
     refute good_input.input_too_short?
   end
 
-  def test_it_rejects_bad_input
+  def test_it_rejects_invalid_input
     bad_input = Input.new("xrrr")
     assert bad_input.invalid_input?
   end
 
+  def test_it_accepts_quit
+    good_input = Input.new("quit")
+    refute good_input.invalid_input?
+  end
+
   def test_it_rejects_input_that_is_too_long
-    skip
     bad_input = Input.new("rrrrr")
     assert bad_input.input_too_long?
   end
 
   def test_it_rejects_input_that_is_too_short
-    skip
     bad_input = Input.new("rrr")
     assert bad_input.input_too_short?
   end
