bc6f1069adabc139955e80942ec6eb5e265bd554
Kalah Cheek <kcheek7@ymail.com>
2014-11-11 15:24:17 -0700

    fixed correct color method. It works now.

--- lib/guess_stats.rb
+++ lib/guess_stats.rb
@@ -1,3 +1,4 @@
+#  DONE
 class GuessStats
   attr_reader :secret_code, :guessed_colors
 
@@ -19,21 +20,18 @@ class GuessStats
 
   def correct_color
     secret_sequence_duplicate = @secret_sequence.dup
-    # guessed_colors_array = guessed_colors.chars
-    # guessed_colors_array.each do |letter|
-    guessed_colors.each do |letter|
+    guessed_colors.each_char do |letter|
       @correct_count += 1 if secret_sequence_duplicate.include?(letter)
-      secret_sequence_duplicate.delete(letter)
+      secret_sequence_duplicate.delete!(letter)
     end
     @correct_count
   end
 end
 
 #
-# stats = GuessStats.new(['r','r','b','b'], 'bbbr')
+# stats = GuessStats.new('bbrg', 'bbbb')
 # stats.correct_color
 # stats.correct_spot_and_color
 #  check input against randomly generated sequence
-#  how many are in the correct spot
 #  how many are the correct color
 #  how many are in the correct spot and color
