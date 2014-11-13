afde4f0880d237fadd6b0b3d1cc5810bf1300753
Kalah Cheek <kcheek7@ymail.com>
2014-11-13 09:19:36 -0700

    added easter egg to allow user to input secret sequence

--- lib/cli.rb
+++ lib/cli.rb
@@ -28,6 +28,7 @@ class CLI
     when play?         then Game.new(instream, outstream, secret_sequence).play
     when instructions? then outstream.puts display.instructions
     when quit?         then outstream.puts display.goodbye_message
+    when input_secret_sequence? then get_the_secret_sequence
     else
       outstream.puts display.invalid_input
     end
@@ -43,6 +44,15 @@ class CLI
 
   def play?
     @user_guess == 'p' || @user_guess == 'play'
+  end
+
+  def input_secret_sequence?
+    @user_guess == "iss"
+  end
+
+  def get_the_secret_sequence
+    outstream.puts "Congratulations, you've found an easter egg! Input your secret sequence."
+    @secret_sequence = instream.gets.strip.downcase
   end
 
 end
